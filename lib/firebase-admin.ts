60df7e5cd58ee94b527049b2b2ea9988f67c7371
ashwink0 <ashwin.kuppahally@gmail.com>
2021-02-28 14:38:41 -0800

    Update firebase-admin.ts

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -4,6 +4,8 @@ import { serviceAccount } from '../credentials';
 
 if (!admin.apps.length) {
 	admin.initializeApp({
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
 		credential: admin.credential.cert(serviceAccount),
 	});
 }
